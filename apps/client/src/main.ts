0b6f87c60dbfc81f1f0a57b2f7a9cb41deaeec7b
vancegillies <vance.gillies@sensand.com>
2024-05-08 20:42:01 +1000

    basic pkce flow in client app

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,7 +1,24 @@
+import { generatePKCE } from "./pkce";
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    Hey
-  </div>
-`;
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const link = setupLink(async (href) => {
+  const code = await generatePKCE();
+  localStorage.setItem("codeVerifier", code.codeVerifier);
+  window.open(`${href}?code_challenge=${code.codeChallenge}`, "_self");
+});
+
+app.appendChild(link);
+
+function setupLink(onClick: (href: string) => void) {
+  const link = document.createElement("a");
+  link.href = "http://localhost:3001/oauth";
+  link.textContent = "Login with OAuth";
+  link.onclick = (e) => {
+    e.preventDefault();
+    onClick(link.href);
+  };
+
+  return link;
+}
